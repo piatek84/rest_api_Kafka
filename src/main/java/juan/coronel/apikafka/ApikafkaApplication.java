0c458dae35ef29464db377035df481dd226488e2
juan coronel <you@example.com>
2022-07-01 22:27:35 +0200

    Initial commit

---
+++ src/main/java/juan/coronel/apikafka/ApikafkaApplication.java
@@ -0,0 +1,13 @@
+package juan.coronel.apikafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApikafkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApikafkaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/juan/coronel/apikafka/ApikafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package juan.coronel.apikafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApikafkaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
