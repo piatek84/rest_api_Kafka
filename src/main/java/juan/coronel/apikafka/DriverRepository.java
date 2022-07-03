77d74a78b8586874ea68b8ca5991c15c3aff8801
juan coronel <you@example.com>
2022-07-03 11:24:58 +0200

    Add empty readme

--- src/main/java/juan/coronel/apikafka/DriverRepository.java
+++
@@ -1,6 +0,0 @@
-package juan.coronel.apikafka;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface DriverRepository extends MongoRepository<Driver, String> {
-}
